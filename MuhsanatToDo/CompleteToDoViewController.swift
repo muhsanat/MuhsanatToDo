45f9bb95393f8bfab28d0d70dbf0054934471985
Muhsanat Chowdhury <muhsanat.chowdhury13@myhunter.cuny.edu>
2022-08-01 10:23:59 -0400

    Initial Commit

---
+++ MuhsanatToDo/CompleteToDoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CompleteToDoViewController.swift
+//  MuhsanatToDo
+//
+//  Created by Scholar on 7/29/22.
+//
+
+import UIKit
+
+class CompleteToDoViewController: UIViewController {
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func completeTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
